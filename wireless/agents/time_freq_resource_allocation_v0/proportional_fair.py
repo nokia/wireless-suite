08cdcf818eb06b28412cad033f46a8778e9e4d77
Darkpudding <48762841+Darkpudding@users.noreply.github.com>
2020-07-06 10:22:17 +0200

    Add ProportionalFairChannelAwareAgent
    - Add ProportionalFairChannelAwareAgent in proportional_fair
    - Move random_agent

--- wireless/agents/time_freq_resource_allocation_v0/proportional_fair.py
+++ wireless/agents/time_freq_resource_allocation_v0/proportional_fair.py
@@ -40,3 +40,39 @@ class ProportionalFairAgent(RandomAgent):
 
         self.t += 1
         return action
+    
+class ProportionalFairChannelAwareAgent(ProportionalFairAgent):
+    def __init__(self, action_space, n_ues, buffer_max_size):
+        super().__init__(action_space, n_ues, buffer_max_size)
+        
+        self._cqi2se = [0.1523,0.2344,0.3770,0.6016,0.8770,1.1758,1.4766,1.9141,2.4063,2.7305,3.3223,3.9023,4.5234,5.1152,5.5547,9.6]
+        
+
+    def act(self, state, reward, done):
+        s = np.reshape(state[self.K:self.K*(1 + self.L)], (self.K, self.L))  # Sizes in bits of packets in UEs' buffers
+        buffer_size_per_ue = np.sum(s, axis=1)
+
+        e = np.reshape(state[self.K*(1 + self.L):self.K*(1 + 2*self.L)], (self.K, self.L))  # Packet ages in TTIs
+        o = np.max(e, axis=1)  # Age of oldest packet for each UE
+
+        qi_ohe = np.reshape(state[self.K + 2 * self.K * self.L:5 * self.K + 2 * self.K * self.L], (self.K, 4))
+        qi = np.array([np.where(r == 1)[0][0] for r in qi_ohe])  # Decode One-Hot-Encoded QIs
+
+        # Extract packet delay budget for all UEs
+        b = np.zeros(qi.shape)
+        b[qi == 3] = 100
+        b[qi == 2] = 150
+        b[qi == 1] = 30
+        b[qi == 0] = 300
+
+        cqi = state[0:self.K]
+        se = np.zeros(shape=(self.K,))
+        for i in range(16):
+            se[cqi == i] = self._cqi2se[i]
+        priorities = (1+o)/b * buffer_size_per_ue * se
+
+        action = np.argmax(priorities)
+        self.n[action] += 1
+
+        self.t += 1
+        return action
